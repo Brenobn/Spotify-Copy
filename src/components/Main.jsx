63b0a00c2ae401c46907cb098b2b83ccb1d6836c
Brenobn <brenobragabn@gmail.com>
2025-02-14 09:54:34 -0300

    Changing the way you use FontAwesomeIcon

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCirclePlay } from '@fortawesome/free-regular-svg-icons'
 
 const Main = () => {
   return (
@@ -13,7 +14,7 @@ const Main = () => {
         <div className='single-item'>
           <img className='single-item__image' src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4" alt="Imagem de Henrique & Juliano" />
 
-          <FontAwesomeIcon icon="fa-solid fa-circle-play" />
+          <FontAwesomeIcon icon={faCirclePlay} />
 
           <p>Henrique & Juliano</p>
           <p>Artista</p>
